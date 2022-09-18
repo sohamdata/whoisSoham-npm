a2a19fb78d36760ebea2e1a1823220f53acf540f
Soham Datta <sohamdatta34@gmail.com>
2022-09-18 12:27:10 +0530

    0

---
+++ index.js
@@ -0,0 +1,29 @@
+import boxen from 'boxen';
+import chalk from 'chalk';
+
+const h = chalk.bold.hex("#A62349");
+const w = chalk.white;
+const g = chalk.hex("#42855B");
+const m = chalk.hex('#6F38C5');
+
+const cardOptions = {
+    padding: 1,
+    margin: 1,
+    borderStyle: 'round',
+    borderColor: "#AD8B73"
+};
+
+const content = `${h("Soham Datta")}
+${w("Computer Science Student from India")}
+${w("Interested in Web Development and learning new things.")}
+
+${h("Technologies")}
+${w("Python, Javascript, Reactjs, C++, Go")}
+
+${g("GitHub")}             ${m("https://github.com/sohamdata")}
+${g("LinkedIn")}           ${m("https://linkedin.com/in/~soham/")}
+${g("Email")}              ${m("sohamdatta34@gmail.com")}`;
+
+const card = boxen(content, cardOptions);
+
+console.log(card);
